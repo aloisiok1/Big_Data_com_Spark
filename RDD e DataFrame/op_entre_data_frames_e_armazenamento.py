50010aae0b0aea3c10a51bc2a0ebd92b522a788d
Aloisio Keulbeck <122841738+aloisiok1@users.noreply.github.com>
2023-09-14 21:42:38 -0300

    14/09/23-2

--- RDD e DataFrame/op_entre_data_frames_e_armazenamento.py
+++ RDD e DataFrame/op_entre_data_frames_e_armazenamento.py
@@ -135,3 +135,44 @@ data = [1,2,3,3,4,5,5,6,7]
 myRDD = sc.parallelize(data)
 myRDD.top(3)
 
+"""## Na Prátrica como é o Apache *Spark*"""
+
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName('Tutorial').getOrCreate()
+
+dept = [("Finance",10),("Marketing",20),("Sales",30),("IT",40)]
+rdd = spark.sparkContext.parallelize(dept)
+
+df = rdd.toDF()
+df.printSchema()
+df.show(truncate=False)
+
+deptColumns = ["dept_name","dept_id"]
+df2 = rdd.toDF(deptColumns)
+df2.printSchema()
+df2.show(truncate=False)
+
+deptDF = spark.createDataFrame(rdd, schema = deptColumns)
+deptDF.printSchema()
+deptDF.show(truncate=False)
+
+from pyspark.sql.types import StructType,StructField, StringType
+deptSchema = StructType([
+    StructField('dept_name', StringType(), True),
+    StructField('dept_id', StringType(), True)
+])
+
+deptDF1 = spark.createDataFrame(rdd, schema = deptSchema)
+deptDF1.printSchema()
+deptDF1.show(truncate=False)
+
+from pyspark.sql.types import StructType,StructField, StringType
+deptSchema = StructType([
+    StructField('dept_name', StringType(), True),
+    StructField('dept_id', StringType(), True)
+])
+
+deptDF1 = spark.createDataFrame(rdd, schema = deptSchema)
+deptDF1.printSchema()
+deptDF1.show(truncate=False)
